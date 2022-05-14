947d5525c06f765b292e58e4023fc7e17e32acdb
ouissemouerdiane <wouerdiane@gmail.com>
2022-05-15 00:57:16 +0200

    Add user command controller test

---
+++ src/test/java/com/microservices/user/command/rest/UserCommandControllerTest.java
@@ -0,0 +1,33 @@
+package com.microservices.user.command.rest;
+
+import com.microservices.user.command.UserAggregate;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(UserCommandController.class)
+public class UserCommandControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserAggregate userAggregate;
+
+    @Test
+    public void deleteUser_OK() throws Exception {
+        doNothing().when(userAggregate).handleDeleteUserCommand(any());
+        mockMvc.perform(delete("/user-management/users/1"))
+                .andExpect(status().isNoContent());
+    }
+}
